af94a1c3cd9308522a60ac55d6036f9aa7156fff
G@&R <gabrielailarreta@gmail.com>
2018-08-17 13:45:44 -0300

    página one en la página de inicio

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/one'
-  get 'pages/two'
+  root 'pages#one'
+  get 'pages/two' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
